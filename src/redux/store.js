9b44c2b52b67173480bfe9392da962b7ffa0372b
django-json <diango.jayson@gmail.com>
2020-06-20 17:35:08 +0800

    setup the redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { persistStore } from "redux-persist";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import logger from "redux-logger";
 
@@ -11,7 +12,10 @@ import rootSaga from "./root-saga";
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [logger, sagaMiddleware];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 /*Persisted store version*/
 const persistor = persistStore(store);
